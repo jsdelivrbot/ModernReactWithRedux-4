8d5bf3e0e9d217779190b08ac1fdc66ef28c0481
An (Martin) Luo <aluo1.developer@gmail.com>
2018-04-01 22:43:35 +1000

    now a list of videos' thumbnails and their titles are rendered

--- ReduxSimpleStarter/src/components/VideoList.js
+++ ReduxSimpleStarter/src/components/VideoList.js
@@ -1,7 +1,11 @@
 import React from "react";
+import VideoListItem from "./VideoListItem";
 
-const VideoList = (props) => {
-  return <ul className="col-md-4 list-group">{props.videos.length}</ul>;
+const VideoList = props => {
+  const videoItems = props.videos.map(video => {
+    return <VideoListItem video={video} key={video.etag} />;
+  });
+  return <ul className="col-md-4 list-group">{videoItems}</ul>;
 };
 
 export default VideoList;
