160f6ea6ceade663fdde7e5806ac72a52e3af27a
An (Martin) Luo <aluo1.developer@gmail.com>
2018-04-01 16:20:38 +1000

    use the installed API to get the result and put it in console.

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/SearchBar";
+import YTSearch from "youtube-api-search";
 
-const API_KEY = 
+const API_KEY = "Placeholder for the Google Youtube API key.";
+
+YTSearch({ key: API_KEY, term: "surfboards" }, function(data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
-  return <div>
+  return (
+    <div>
       <SearchBar />
-  </div>;
+    </div>
+  );
 };
 
 // Take this component's generated HTML and put it on the page (in the DOM).
