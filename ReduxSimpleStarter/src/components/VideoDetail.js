a20e82189074bed3f43044327c1a3dc51dd626f7
An (Martin) Luo <aluo1.developer@gmail.com>
2018-04-02 21:35:11 +1000

    handling null props

--- ReduxSimpleStarter/src/components/VideoDetail.js
+++ ReduxSimpleStarter/src/components/VideoDetail.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>
+  }
+  
   const videoTitle = video.snippet.title;
   const videoDescription = video.snippet.description;
   const videoID = video.id.videoId;
