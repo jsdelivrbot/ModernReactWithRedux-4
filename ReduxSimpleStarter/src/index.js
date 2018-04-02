e3773ab28ec33df26f90d8fafd2302f7d445974f
An (Martin) Luo <aluo1.developer@gmail.com>
2018-04-02 22:19:34 +1000

    refactor the code, set throttling about user input

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import _ from "lodash";
 import SearchBar from "./components/SearchBar";
 import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
 import YTSearch from "youtube-api-search";
 
 const API_KEY = "Placeholder for the Google Youtube API key.";
@@ -9,19 +11,42 @@ const API_KEY = "Placeholder for the Google Youtube API key.";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
-      this.setState({ videos });
-      // === this.setState({ videos: videos });
-    });
+    this.videoSearch("surfboards");
+  }
+
+  videoSearch(term) {
+    YTSearch(
+      {
+        key: API_KEY,
+        term: term
+      },
+      videos => {
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        });
+      }
+    );
   }
 
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        />
       </div>
     );
   }
