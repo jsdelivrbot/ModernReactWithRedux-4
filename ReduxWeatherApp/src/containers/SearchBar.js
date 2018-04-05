39aa143fd45278112ae4d5834258a8068e6fe72c
An (Martin) Luo <aluo1.developer@gmail.com>
2018-04-05 21:18:47 +1000

    make input in search bar a controlled component, add event handlers

--- ReduxWeatherApp/src/containers/SearchBar.js
+++ ReduxWeatherApp/src/containers/SearchBar.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ""
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form className="input-group" onSubmit={this.handleFormSubmit}>
+        <input
+          placeholder="Get a five-day forecast in your favourite cities."
+          className="form-control"
+          value={this.state.term}
+          onChange={this.handleInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">
             Submit
