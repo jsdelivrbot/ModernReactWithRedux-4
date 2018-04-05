932489d13242eee1d4eec9fa9cda3659db2b2074
An (Martin) Luo <aluo1.developer@gmail.com>
2018-04-05 21:50:18 +1000

    create an action creator to fetch weather

---
+++ ReduxWeatherApp/src/actions/index.js
@@ -0,0 +1,13 @@
+import { API_URL } from "../constants/API";
+import axios from "axios";
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+const fetchWeather = city => {
+  const completeUrl = `${API_URL}&q=${city},au`;
+  const request = axios.get(completeUrl);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
