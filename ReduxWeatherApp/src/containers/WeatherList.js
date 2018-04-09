015961cff17d6df7d0cf7cf3d77fb20030e8d323
An (Martin) Luo <aluo1.developer@gmail.com>
2018-04-09 23:02:18 +1000

    render weather with chart

--- ReduxWeatherApp/src/containers/WeatherList.js
+++ ReduxWeatherApp/src/containers/WeatherList.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Chart from "../components/Chart";
 
 class WeatherList extends Component {
+  renderWeather = cityData => {
+    const name = cityData.city.name;
+    const dataList = cityData.list;
+    const temps = dataList.map(weather => {
+      return weather.main.temp;
+    });
+    const pressures = dataList.map(weather => {
+      return weather.main.pressure;
+    });
+    const humidities = dataList.map(weather => {
+      return weather.main.humidity;
+    });
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color={"red"} />
+        </td>
+        <td>
+          <Chart data={pressures} color="green" />
+        </td>
+        <td>
+          <Chart data={humidities} color="blue" />
+        </td>
+      </tr>
+    );
+  };
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperatur</th>
+            <th>Temperature</th>
             <th>Pressure</th>
             <th>Humidity</th>
           </tr>
         </thead>
-        <tbody />
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
       </table>
     );
   }
